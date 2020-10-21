7f2545df0ace154f344ae8959055bc43a62289fd
Andy Fragen <andy@thefragens.com>
2020-10-21 11:30:20 -0700

    cleanup and bump

--- src/WPBT/WP_Beta_Tester.php
+++ src/WPBT/WP_Beta_Tester.php
@@ -128,8 +128,8 @@ class WP_Beta_Tester {
 		// It's a core-update request.
 		$args['_beta_tester'] = true;
 
+		// $url = str_replace( 'version=' . $wp_version, 'version=' . $this->get_current_wp_release(), $url );
 		$wp_version = get_bloginfo( 'version' );
-		//$url        = str_replace( 'version=' . $wp_version, 'version=' . $this->get_current_wp_release(), $url );
 		$url        = empty( self::$options['stream-option'] )
 			? add_query_arg( 'channel', self::$options['channel'], $url )
 			: add_query_arg( 'channel', self::$options['stream-option'], $url );
