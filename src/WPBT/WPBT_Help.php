c13a2251406accddfabb88d65131dac51780bd12
Andy Fragen <andy@thefragens.com>
2020-10-21 11:29:42 -0700

    update help

--- src/WPBT/WPBT_Help.php
+++ src/WPBT/WPBT_Help.php
@@ -44,11 +44,12 @@ class WPBT_Help {
 				'content' => '<p>' .
 				sprintf(
 					/* translators: 1: link to backing up database, 2: link to make.wp.org/core, 3: link to beta support forum */
-					__( 'By their nature, these releases are unstable and should not be used anyplace where your data is important. So please <a href="%1$s">back up your database</a> before upgrading to a test release. In order to hear about the latest beta releases, your best bet is to watch the <a href="%2$s">development blog</a> and the <a href="%3$s">beta forum</a>.', 'wordpress-beta-tester' ),
+					__( 'By their nature, these releases are unstable and should not be used any place where your data is important. So please <a href="%1$s">back up your database</a> before upgrading to a test release. In order to hear about the latest beta releases, your best bet is to watch the <a href="%2$s">development blog</a> and the <a href="%3$s">beta forum</a>.', 'wordpress-beta-tester' ),
 					_x( 'https://codex.wordpress.org/Backing_Up_Your_Database', 'URL to database backup instructions', 'wordpress-beta-tester' ),
 					'https://make.wordpress.org/core/',
 					_x( 'https://wordpress.org/support/forum/alphabeta', 'URL to beta support forum', 'wordpress-beta-tester' )
-				) . '</p>',
+				) . '</p><p>' .
+				sprintf( __( 'When switching between <strong>Point release</strong> and <strong>Bleeding edge</strong> you will need to set the stream option to <strong>Nightlies</strong> then update to get on that channel, then set to another stream option ( <strong>Beta/RC Only</strong> or <strong>Release Candidates Only</strong> ) if desired.', 'wordpress-beta-tester' ) ) . '</p>',
 			)
 		);
 
