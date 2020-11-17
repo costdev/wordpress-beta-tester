798ba28c1a8b005ea4977ffff4acf9ef2bbe787c
Andy Fragen <andy@thefragens.com>
2020-11-17 10:38:10 -0800

    composer update

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -14,33 +14,33 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
     'aliases' => 
     array (
     ),
-    'reference' => '53c309955a1b9d62c54066a5e429d30b54750337',
+    'reference' => '2cb33fee0f9cfe3e96a486d60ff4b2bc07f2ce08',
     'name' => 'afragen/wordpress-beta-tester',
   ),
   'versions' => 
   array (
     'afragen/wordpress-beta-tester' => 
     array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
       'aliases' => 
       array (
       ),
-      'reference' => '53c309955a1b9d62c54066a5e429d30b54750337',
+      'reference' => '2cb33fee0f9cfe3e96a486d60ff4b2bc07f2ce08',
     ),
     'wp-cli/wp-config-transformer' => 
     array (
-      'pretty_version' => 'v1.2.7',
-      'version' => '1.2.7.0',
+      'pretty_version' => 'v1.2.8',
+      'version' => '1.2.8.0',
       'aliases' => 
       array (
       ),
-      'reference' => 'bae1e975ed1277470e3dcc7fd0ef99c2d4f4c7a8',
+      'reference' => '0bb2b9162c38ca72370380aea11dc06e431e13a5',
     ),
   ),
 );
