f252ac72a672f1548e8f71d9022d2c3c58dd56a6
Andy Fragen <andy@thefragens.com>
2021-02-03 13:53:33 -0800

    a little too aggressive

--- src/WPBT/WPBT_Core.php
+++ src/WPBT/WPBT_Core.php
@@ -391,8 +391,9 @@ class WPBT_Core {
 		$current_exploded = array_map( 'intval', explode( '.', $exploded_version[0] ) );
 		if ( 'development' === self::$options['channel'] && isset( $current_exploded[2] ) ) {
 			$current_exploded[1] = ++$current_exploded[1];
-			unset( $current_exploded[2], $exploded_version[1], $exploded_version[2] );
+			unset( $current_exploded[2] );
 			$exploded_version[0] = implode( '.', $current_exploded );
+			$exploded_version[1] = 'alpha';
 		}
 
 		preg_match( '/beta(\d+)/', $exploded_version[1], $current_beta );
