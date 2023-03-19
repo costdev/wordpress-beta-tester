571b0a5a2a149c11778a7a378ca165f167b90f4e
Colin Stewart <79332690+costdev@users.noreply.github.com>
2023-03-19 21:40:08 +0000

    `Bug Report`: Store plugin version as static property and move CSS/JS to files. (#63)
    * `Bug Report`: Store plugin version in static property.
    
    * `Bug Report`: Store the plugin's base URL in a static property.
    
    * `Bug Report`: Introduce admin bar stylesheet.
    
    * `Bug Report`: Introduce template stylesheet.
    
    * `Bug Report`: Introduce clipboard script.
    
    * `Bug Report`: Implement scripts and stylesheets.

--- src/WPBT/WPBT_Bug_Report.php
+++ src/WPBT/WPBT_Bug_Report.php
@@ -20,6 +20,20 @@ class WPBT_Bug_Report {
 	protected static $options;
 
 	/**
+	 * Holds the plugin's version.
+	 *
+	 * @var string
+	 */
+	protected static $plugin_version;
+
+	/**
+	 * Holds the plugin's base URL.
+	 *
+	 * @var string
+	 */
+	protected static $plugin_base_url;
+
+	/**
 	 * Holds the WP_Beta_Tester instance.
 	 *
 	 * @var WP_Beta_Tester
@@ -99,10 +113,14 @@ class WPBT_Bug_Report {
 	 */
 	public function __construct( WP_Beta_Tester $wp_beta_tester, $options ) {
 		require_once ABSPATH . 'wp-admin/includes/plugin.php';
-		$this->wp_beta_tester = $wp_beta_tester;
-		self::$options        = $options;
-		self::$unknown        = __( 'Could not determine', 'wordpress-beta-tester' );
-		self::$none_activated = __( 'None activated', 'wordpress-beta-tester' );
+		$this->wp_beta_tester  = $wp_beta_tester;
+		self::$plugin_version  = get_file_data( $this->wp_beta_tester->file, array( 'Version' => 'Version' ) )['Version'];
+		$directory             = basename( dirname( $this->wp_beta_tester->file ) );
+		$file                  = basename( $this->wp_beta_tester->file );
+		self::$plugin_base_url = plugin_dir_url( $directory . '/' . $file );
+		self::$options         = $options;
+		self::$unknown         = __( 'Could not determine', 'wordpress-beta-tester' );
+		self::$none_activated  = __( 'None activated', 'wordpress-beta-tester' );
 	}
 
 	/**
@@ -124,60 +142,31 @@ class WPBT_Bug_Report {
 	 * @return void
 	 */
 	public function enqueue_scripts() {
-		$version = get_file_data( $this->wp_beta_tester->file, array( 'Version' => 'Version' ) )['Version'];
-
-		wp_register_style( 'wordpress-beta-tester-bug_report', '', '', $version );
-		wp_enqueue_style( 'wordpress-beta-tester-bug_report' );
-		wp_add_inline_style(
-			'wordpress-beta-tester-bug_report',
-			'
-			#wp-admin-bar-wp-beta-tester-report-a-bug {
-				display: block !important;
-			}
-
-			#wp-admin-bar-wp-beta-tester-report-a-bug .ab-icon {
-				margin: 2px 4px 0 0;
-			}
-
-			#wp-admin-bar-wp-beta-tester-report-a-bug .ab-icon::before {
-				content: "\f451";
-			}
-
-			@media (max-width: 782px) {
-				#wp-admin-bar-wp-beta-tester-report-a-bug .ab-icon {
-					margin-left: 4px;
-				}
-
-				#wp-admin-bar-wp-beta-tester-report-a-bug .ab-label {
-					display: none;
-				}
-			}
-			'
+		wp_enqueue_style(
+			'wordpress-beta-tester-bug-report-admin-bar',
+			self::$plugin_base_url . 'src/WPBT/css/bug-report-admin-bar.css',
+			array(),
+			self::$plugin_version
 		);
 
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		if ( ! isset( $_GET['tab'] ) || 'wp_beta_tester_bug_report' !== $_GET['tab'] ) {
-			return;
+		if ( is_admin() && isset( $_GET['tab'] ) && 'wp_beta_tester_bug_report' === $_GET['tab'] ) {
+			wp_enqueue_style(
+				'wordpress-beta-tester-bug-report-template',
+				self::$plugin_base_url . 'src/WPBT/css/bug-report-template.css',
+				array(),
+				self::$plugin_version
+			);
+
+			wp_enqueue_script(
+				'wordpress-beta-tester-bug-report-clipboard',
+				self::$plugin_base_url . 'src/WPBT/js/bug-report-clipboard.js',
+				array( 'jquery', 'clipboard' ),
+				self::$plugin_version,
+				true
+			);
 		}
 
-		wp_register_script( 'wordpress-beta-tester-bug_report', '', array( 'jquery', 'clipboard' ), $version, true );
-		wp_enqueue_script( 'wordpress-beta-tester-bug_report' );
-		wp_add_inline_script(
-			'wordpress-beta-tester-bug_report',
-			'
-				var bugReportClipboard = new ClipboardJS( "#wordpress-beta-tester-bug-reports button" );
-
-				bugReportClipboard.on( "success", function( e ) {
-					var success = jQuery( e.trigger ).next( ".success" );
-
-					success.removeClass( "hidden" );
-
-					setTimeout( function() {
-						success.addClass( "hidden" );
-					}, 3000 );
-				} );
-			'
-		);
 	}
 
 	/**
@@ -528,18 +517,18 @@ class WPBT_Bug_Report {
 	public function print_bug_report_template( $title, $url, $format ) {
 			$test_report = $this->get_bug_report_template( $format );
 		?>
-		<div style="width: min( 100vw, 520px - .5rem); padding-bottom: 2rem;">
+		<div class="template">
 			<h2><?php echo esc_html( $title ); ?></h2>
-			<div style="display: flex; align-items: center; gap: 1rem;">
+			<div class="template-buttons">
 				<a class="button button-small" href="<?php echo esc_url( $url ); ?>" target="_blank"><?php esc_html_e( 'File a report', 'wordpress-beta-tester' ); ?></a>
-				<div style="display: flex; align-items: center; gap: .25rem;">
+				<div class="copy-to-clipboard">
 					<button type="button" class="button button-small" data-clipboard-text="<?php echo esc_attr( str_replace( '&nbsp;', ' ', $test_report ) ); ?>">
 						<?php esc_html_e( 'Copy to clipboard', 'wordpress-beta-tester' ); ?>
 					</button>
-					<span class="success hidden" style="color: #008a20;" aria-hidden="true"><?php esc_html_e( 'Copied!', 'wordpress-beta-tester' ); ?></span>
+					<span class="success hidden" aria-hidden="true"><?php esc_html_e( 'Copied!', 'wordpress-beta-tester' ); ?></span>
 				</div>
 			</div>
-			<?php echo wp_kses_post( '<div class="card" style="margin-top: 1rem;">' . nl2br( $this->get_bug_report_template( $format ) ) . '</div>' ); ?>
+			<?php echo wp_kses_post( '<div class="card">' . nl2br( $this->get_bug_report_template( $format ) ) . '</div>' ); ?>
 		</div>
 		<?php
 	}
